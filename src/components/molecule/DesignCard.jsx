e63306ebb3e3737f0096c1aab6bb39ef83810e5c
yeoseoyun <yeosummer94@gmail.com>
2024-05-06 21:46:28 +0900

    디자인 카드 이미지 추가

--- src/components/molecule/DesignCard.jsx
+++ src/components/molecule/DesignCard.jsx
@@ -3,8 +3,12 @@ import DetailLink from "../atom/DetailLink";
 function DesignCard({ title, imgSrc, imgAlt, detailLink }) {
   return (
     <>
-      <div className="w-full flex flex-col items-center gap-3">
-        <img src={imgSrc} alt={imgAlt} className="rounded-xl" />
+      <div className="w-full h-full flex flex-col items-center gap-3">
+        <img
+          src={imgSrc}
+          alt={imgAlt}
+          className="rounded-xl w-full h-full object-cover"
+        />
         <h2>{title}</h2>
         <DetailLink to={detailLink}>자세히 보기</DetailLink>
       </div>
--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,4 +1,7 @@
+import sylogo from "../assets/MockUp/seoyunlogo.webp";
 import muzigae from "../assets/MockUp/muzigae.webp";
+import dalba from "../assets/MockUp/dalba.webp";
+import bluesquare from "../assets/MockUp/bluesquare.webp";
 import DesignCard from "../components/molecule/DesignCard";
 
 function Design() {
@@ -6,22 +9,34 @@ function Design() {
     <>
       <div
         id="design"
-        className="w-full h-screen py-10 px-20 flex flex-col justify-center gap-10"
+        className="w-full h-[130vh] py-10 px-20 flex flex-col justify-center gap-10"
       >
         <h2 className="text-5xl font-bold">PORTFOLIO-DESIGN</h2>
-        <div className="w-full h-full grid grid-cols-3 gap-6 bg-slate-100">
+        <div className="w-full h-full grid grid-cols-3 gap-8">
+          <DesignCard
+            title="서윤 이니셜 로고"
+            imgSrc={sylogo}
+            imgAlt="서윤 이니셜로고"
+            detailLink="/"
+          />
           <DesignCard
             title="FITTING BLUSH BANNER"
             imgSrc={muzigae}
             imgAlt="피팅블러셔 배너"
             detailLink="/"
           />
-          <div>
-            <img src={muzigae} alt="화장품 배너" className="rounded-xl" />
-          </div>
-          <div>
-            <img src={muzigae} alt="화장품 배너" className="rounded-xl" />
-          </div>
+          <DesignCard
+            title="HANDCRAM BANNER"
+            imgSrc={dalba}
+            imgAlt="핸드크림 배너"
+            detailLink="/"
+          />
+          <DesignCard
+            title="BLUESQUARE UI/UX"
+            imgSrc={bluesquare}
+            imgAlt="블루스퀘어 ui/ux"
+            detailLink="/"
+          />
         </div>
       </div>
     </>
