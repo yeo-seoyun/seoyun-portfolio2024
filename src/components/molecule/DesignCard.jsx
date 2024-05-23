5762cb6578efd20f92ccdccb019367d5e6da22ff
yeoseoyun <yeosummer94@gmail.com>
2024-05-24 01:02:59 +0900

    design css 수정

--- src/components/molecule/DesignCard.jsx
+++ src/components/molecule/DesignCard.jsx
@@ -3,11 +3,11 @@ import DetailLink from "../atom/DetailLink";
 function DesignCard({ title, imgSrc, imgAlt, detailLink }) {
   return (
     <>
-      <div className="w-full h-full flex flex-col items-center justify-between gap-3 group overflow-hidden">
+      <div className="w-full h-full flex flex-col items-center justify-between gap-3 group">
         <img
           src={imgSrc}
           alt={imgAlt}
-          className="rounded-xl w-full h-full object-cover group-hover:drop-shadow-md transition-all"
+          className="rounded-xl w-full h-full group-hover:drop-shadow-md transition-all"
         />
         <h2 className="text-xl">{title}</h2>
         <DetailLink to={detailLink}>자세히 보기</DetailLink>
