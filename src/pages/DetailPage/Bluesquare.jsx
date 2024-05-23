801a39d890353aed9d3a1f414c30fe60197b06b0
yeoseoyun <yeosummer94@gmail.com>
2024-05-23 19:56:40 +0900

    반응형 뷰 수정

--- src/pages/DetailPage/Bluesquare.jsx
+++ src/pages/DetailPage/Bluesquare.jsx
@@ -4,17 +4,19 @@ import ProjectTitleDesign from "../../components/atom/ProjectTitleDesign";
 function Bluesquare() {
   return (
     <>
-      <div className="w-full p-20 flex flex-col gap-20">
-        <div className="flex flex-col gap-5 pt-10">
-          <ProjectTitleDesign
-            title="BLUEAQUARE MOBILE UX/UI DESIGN"
-            designContribution="100"
-            description="블루스퀘어 사이트 모바일 뷰 디자인<br /> "
-          />
-        </div>
+      <div className="w-full py-28 flex flex-col m-auto gap-28 md:w-[768px] sm:w-[320px] sm:gap-14">
+        <ProjectTitleDesign
+          title="BLUEAQUARE MOBILE UX/UI DESIGN"
+          designContribution="100"
+          description="블루스퀘어 사이트 모바일 뷰 디자인<br /> "
+        />
         <div className="max-w-[1280px] m-auto space-y-10">
           <div className="flex">
-            <img src={mockup} alt="목업이미지" className="h-screen m-auto" />
+            <img
+              src={mockup}
+              alt="목업이미지"
+              className="h-screen m-auto sm:h-[50vh]"
+            />
           </div>
         </div>
       </div>
