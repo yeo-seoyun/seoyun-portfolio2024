989597e9ff3a550703206b3e54444ecc301e7e82
yeoseoyun <yeosummer94@gmail.com>
2024-05-24 16:03:40 +0900

    svg 이미지 파일 수정, 무지개 맨션 디테일 페이지 수정

--- src/pages/DetailPage/Muzigae.jsx
+++ src/pages/DetailPage/Muzigae.jsx
@@ -17,7 +17,6 @@ function Muzigae() {
     { name: "HTML", image: html },
     { name: "CSS", image: css },
     { name: "JavaScript", image: javascript },
-    { name: "React", image: react },
     { name: "Tailwind", image: tailwind },
     { name: "Pocketbase", image: pocketbase },
     { name: "Vite", image: vite },
@@ -35,8 +34,8 @@ function Muzigae() {
             githubLink="https://github.com/yeo-seoyun/muzigae-mansion"
             projectImage={safari}
             description="무지개맨션 홈페이지는 현재 메인 페이지만 개발이 완료된 프로젝트로, <br /> 
-            사용자에게 효과적인 첫 인상을 제공하기 위해 디자인되었습니다. <br /> 
-            React, Vite 및 Tailwind CSS를 활용하여 현대적인 웹 디자인 기준과 반응형 기능을 지원하는 사이트를 구현하였습니다."
+            사용자에게 효과적인 첫 인상을 제공하기 위해 디자인되었습니다. <br /> Javascript를 사용하였고,
+            Vite 및 Tailwind CSS를 활용하여 현대적인 웹 디자인 기준과 반응형 기능을 지원하는 사이트를 구현하였습니다."
           />
 
           <div className="w-full border-b"></div>
